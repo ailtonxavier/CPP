285c21fb9f0638895f0104b29ea791a4505343bf
Ailton Xavier <ailton.xavier@hotmail.com.br>
2022-04-30 15:50:50 -0300

    version 0.0.3

--- lista3/exer_13.cpp
+++ lista3/exer_13.cpp
@@ -1,7 +1,7 @@
 /*
 Elabore  um  algoritmo  que  leia  um  número  inteiro  qualquer  e  verifique  se  ele  é  quadrangular.  Se  for, 
 mostrar os próximos 10 quadrangulares depois dele. Se não for, informar. OBS: Os números 
-quadrangulares são 1, 4, 9, 16, 25, ..., ou seja, 1x1, 2x2, 3x3, 4x4, ... 
+quadrangulares são 1, 4, 9, 16, 25, ..., ou seja, 1x1, 2x2, 3x3, 4x4, ...
 */
 
 #include <iostream>
@@ -14,7 +14,7 @@ int main(){
     cin >> a;
     b = (b * b) == a;
     cout << b;
-    
+    return 0;
 
 
 
--- lista3/exer_14.cpp
+++ lista3/exer_14.cpp
@@ -8,34 +8,45 @@ resultado. O algoritmo só deverá finalizar quando o usuário escolher a opçã
 #include <iostream>
 using namespace std;
 int main(){
-    int menu = 1, base, altura, raio, triangulo, quadrado, retangulo, trapezio, circulo, resultado;
+    int menu = 1;
+    float base, altura, raio, triangulo, quadrado, retangulo, trapezio, circulo, resultado, base_menor;
     while(menu != 6){
-        triangulo = false, quadrado = false, retangulo = false, trapezio = false, circulo = false, resultado = 0;
         cout << "--------------------\n        MENU        \n--------------------\n1 - Triangulo\n2 - Quadrado\n3 - Retangulo\n4 - Trapezio\n5 - Circulo\n6 - Sair\n--------------------\nDigite sua opcao: ";
         cin >> menu;
 
         switch(menu){
-            case 1: triangulo = true; break;
-            case 2: quadrado = true; break;
-            case 3: retangulo = true; break;
-            case 4: trapezio = true; break;
-            case 5: circulo = true; break;
+            case 1:
+                cout << "Digite o lado da base: ";
+                cin >> base;
+                cout << "Digite o lado da altura: ";
+                cin >> altura;
+                resultado = (base * altura)/2;
+                cout << "A area do triangulo eh " << resultado << "m2" << endl; break;
+            case 2: 
+                cout << "Digite o lado da base: ";
+                cin >> base;
+                resultado = base * base;
+                cout << "A area do triangulo eh " << resultado << "m2" << endl; break;
+            case 3:
+                cout << "Digite o lado da base: ";
+                cin >> base;
+                cout << "Digite o lado da altura: ";
+                cin >> altura;
+                resultado = base * altura;
+                cout << "A area do retangulo eh " << resultado << "m2" << endl; break;
+            case 4:
+                cout << "Digite o lado da base maior: ";
+                cin >> base;
+                cout << "Digite o lado da base menor: ";
+                cin >> base_menor;
+                cout << "Digite o lado da altura: ";
+                cin >> altura;
+                resultado = ((base + base_menor) * altura) / 2;
+                cout << "A area do retangulo eh " << resultado << "m2" << endl; break;
+            case 5: 
+                cout << "Digite o raio do circulo: ";
+                cin >> raio; break;
             case 6: menu = 6; break;
         }
-        if(circulo){
-            cout << "Digite o raio do circulo: ";
-
-            cin >> raio;
-        } else {
-            cout << "Digite o lado da base: ";
-            cin >> base;
-            cout << "Digite o lado da altura: ";
-            cin >> altura;
-            if(triangulo){
-                resultado = (base * altura)/2;
-                cout << "A area do triangulo eh " << resultado << "m2" << endl;
-            }
-        }
-
     }
 }
