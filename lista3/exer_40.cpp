b55061fbb4ca4dea64d05676f9c2c4ac1ff0f723
Ailton Xavier <ailton.xavier@hotmail.com.br>
2022-04-30 01:43:33 -0300

    update version 0.0.2

--- lista3/exer_40.cpp
+++ lista3/exer_40.cpp
@@ -48,17 +48,24 @@ int main(){
                 case 4: votosBranco++; break;
                 case 5: votosNulo++; break;
             } totalDeVotos++;
-            if((menu > 0)&& (menu >= 3)){
+            if((menu > 0) && (menu <= 3)){
                 totalDeVotosValidos++;
             }
         }
     }
-    cout <<  "\nTotal de votos do candidato 1: " << votosCandidato1 << "\nTotal de votos do candidato 2: "<< votosCandidato2 << "\nTotal de votos do candidato 3: " << votosCandidato3 << "\nTotal de votos branos: " << votosBranco << "\nTotal de votos nulos: " << votosNulo << "\nTotal de Votos - " << totalDeVotos << endl;
+    cout <<  "\nTotal de votos do candidato 1: " << votosCandidato1 << "\nTotal de votos do candidato 2: "<< votosCandidato2 << "\nTotal de votos do candidato 3: " << votosCandidato3 << "\nTotal de votos branos: " << votosBranco << "\nTotal de votos nulos: " << votosNulo << "\nNumero total de Votos - " << totalDeVotos << endl << endl;
 
     if(votosCandidato1 > (totalDeVotosValidos / 2)){
         cout << "Parabens candidato 1";
     } else if(votosCandidato2 > (totalDeVotosValidos / 2)){
         cout << "Parabens candidato 2";
+    } else if(votosCandidato3 > (totalDeVotosValidos / 2)){
+        cout << "parabens candidato 3";
+    } else if((votosBranco + votosNulo) >= totalDeVotosValidos){
+        cout << "Democracia representativa eh uma piada, topo politico rouba.";
+    }
+    else {
+        cout << "Ninguem atingiu mais que 50% dos votos.";
     }
     
 }
